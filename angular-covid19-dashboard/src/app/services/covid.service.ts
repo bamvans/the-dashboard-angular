8877974eb33458a97968d96774da7b7798615ff7
Stacey <stacey@digitalnotion.co.uk>
2021-11-30 10:49:36 +0200

    Created Service.

--- angular-covid19-dashboard/src/app/services/covid.service.ts
+++ angular-covid19-dashboard/src/app/services/covid.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CovidService {
+  private overallSubject = new Subject<any>();
+  private latestSubject = new Subject<any>();
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  private get(url: string): Observable<any> {
+    return this.httpClient.get(`${environment.baseUrl}/${url}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  callOverall() {
+    this.get('timeline-overall.json').subscribe((x) => {
+      this.overallSubject.next(x);
+    });
+  }
+
+  getOverall(): Observable<any> {
+    return this.overallSubject.asObservable();
+  }
+
+  callLatest() {
+    this.get('timeline-latest.json').subscribe((x) => {
+      this.latestSubject.next(x);
+    });
+  }
+
+  getLatest(): Observable<any> {
+    return this.latestSubject.asObservable();
+  }
 }
